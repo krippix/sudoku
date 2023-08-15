f241b7f26b2fa02d56163df18cfcf2a4f7f12ab0
krippix <krippix@user.noreply.github.com>
2023-08-15 17:39:28 +0200

    implemented vertical collision check

--- src/game.asm
+++ src/game.asm
@@ -17,10 +17,11 @@ find_collisions proc
     ; cl = horizontal | ch = vertical
     call box_to_row
 
-    ; find collisions in horizontal row
+    ; find collisions
     call find_collisions_hor
+    call find_collisions_vert
 
-    ; find collisions in vertical row
+    ; determine current cube
     ; find collsions in current cube
 
     mov last_modified, 0FFh ; reset last modified to none
@@ -50,11 +51,6 @@ find_collisions_hor proc
     mov bx, 9
     mul bx
     mov di, ax
-
-    push dx
-    mov dx, di
-    call draw_dx
-    pop dx
 
     ; iterate over the row
     ; make ax the upper bound
@@ -107,14 +103,77 @@ find_collisions_hor proc
     ret
 find_collisions_hor endp
 
+; marks collisions in provided row
+; ch = row to check for collisions
+; RETURN:
+; nothing
 find_collisions_vert proc
+    push ax
+    push bx     ; holds numbers without data
+    push cx     ; holds bytes to compare
+    push di     ; current box
+    push si     ; box to compare to
+
+    xor ax, ax
+    mov al, ch
+
+    ; top-most box in the row
+    mov di, ax
+
+    ; make ax the upper bound
+    add ax, 72
+
+    @@outer_loop:
+    mov si, di
+    add si, 9
+
+    mov cl, [fields+di] ; load byte and isolate number
+    mov bl, cl
+    and bl, 00001111b
+
+    @@inner_loop:
+    mov ch, [fields+si] ; load byte and isolate number
+    mov bh, ch
+    and bh, 00001111b
+
+    cmp bh, bl
+    jne @@not_equal
+
+    or cl, 01000000b
+    or ch, 01000000b
+
+    mov [fields+di], cl
+    mov [fields+si], ch
+
+    ; draw changed boxes
+    push cx
+    mov cx, di
+    call draw_box
+    mov cx, si
+    call draw_box
+    pop cx
+
+    @@not_equal:
+    add si, 9
+    cmp si, ax
+    jle @@inner_loop
+    add di, 9
+    cmp di, ax
+    jl @@outer_loop
+
+    push dx
+    mov dx, si
+    call draw_dx
+    pop dx
+
+    @@return:
+    pop si
+    pop di
+    pop cx
+    pop bx
+    pop ax
     ret
 find_collisions_vert endp
-
-; checks if collisions were resolved
-resolve_collision proc
-    ret
-resolve_collision endp
 
 ; takes boxnumber and returns horizontal row number [0-8]
 ; bx = boxnumber
@@ -140,3 +199,13 @@ box_to_row proc
     ret
 box_to_row endp
 
+; determine cube nr from box nr
+; bl = cube nr
+box_to_cube proc
+    push cx
+
+    TODO
+
+    pop cx
+    ret
+box_to_cube endp
